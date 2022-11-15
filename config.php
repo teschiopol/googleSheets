150749012105c493eb94c429cb244292dadc1f19
Pol <48021053+teschiopol@users.noreply.github.com>
2022-11-15 12:51:40 +0100

    feat: config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+// After install composer
+require 'vendor/autoload.php';
+
+$client = new \Google_Client();
+$client->setApplicationName('Google Sheet');
+$client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
+$client->setAccessType('offline');
+$client->setAuthConfig(__DIR__.'/credentials.json');
+$service = new \Google_Service_Sheets($client);
+$spreadSheetId = 'ID_SPECIFIC_GOOGLE_SHEET_FIND_IN_URL';
